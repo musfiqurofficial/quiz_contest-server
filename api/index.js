00d99e3f0556a71ee4602398d8f8ad49932524b2
musfiqurofficial <musfiqurofficial1999@gmail.com>
2025-10-03 10:01:47 +0600

    new code

--- api/index.js
+++ api/index.js
@@ -1,4 +1,3 @@
-const { createServer } = require('http');
 const mongoose = require('mongoose');
 const app = require('../dist/app');
 
@@ -14,25 +13,5 @@ if (process.env.DATABASE_URL) {
     });
 }
 
-// Create server
-const server = createServer(app);
-
-// Export the serverless function
-module.exports = (req, res) => {
-  // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, OPTIONS',
-  );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-  // Handle preflight requests
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-
-  // Handle the request
-  server.emit('request', req, res);
-};
+// Export the Express app as a serverless function
+module.exports = app;
